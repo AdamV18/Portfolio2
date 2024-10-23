91dc6ccf41bbb29403952cec21ca605ce593e4ee
sward <adam.vanston@gmail.com>
2024-10-23 19:56:46 +0200

    nothing i think changed

--- src/main/java/com/example/portfolio2/HelloApplication.java
+++ src/main/java/com/example/portfolio2/HelloApplication.java
@@ -101,7 +101,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Choose your fighter !");
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
